388865f01c4e40ae80f0bcf08a2f9fd5e1d68be1
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-09-22 23:58:31 -0700

    Begin furthering server features.

--- server.js
+++ server.js
@@ -27,18 +27,32 @@ app.get('/', (request, response) => {
 // not sure if the weather stuff works yet
 
 
-app.get("/weather", (request, response) => {
+app.get('/weather', async (request, response) => {
 
-  const cityName = request.query.cityName;
-  const weatherForecast = weather.find(city => city.city_name === cityName);
+  const cityWeather = request.query.cityName;
+  const weatherForecast = weather.find(city => city.city_name === cityWeather);
+  const lat = request.query.lat;
+  const lon = request.query.lon;
+
+  const weatherData = await axios.get(`https://api.weatherbit.io/v2.0/forecast/daily/?city=${cityWeather}&key=${process.env.WEATHER_API_KEY}`);
+  
  
   if(weatherForecast) {
-    const weatherArray = weatherForecast.data.map(result => new Forecast(result));
-    response.send(weatherArray);
+    const weatherData = weatherForecast.data.map(result => new Forecast(result));
+    response.send(weatherData);
   } else {
-    response.status(500).send('Invalid Reponse. Please enter new city.')
+    response.status(500).send('Invalid Reponse. Please enter new city.');
   }
 });
+
+app.get('/movies', async (request, resonse)) => {
+  const cityMovies = request.query.cityMovies;
+  const movieData = await axios.get(`https://api.themoviedb.org/3/search/movie?query=${cityName}&api_key=${process.env.MOVIES_API_KEY}&language=en-US&page=1&include_adult=false`);
+
+}
+
+
+
 
 app.listen(PORT, () => console.log(`Listening on PORT ${PORT}`));
 
